fd0e26ed923f1d1a7101f1c8cd03e3f00ddf8569
Damien BEAUDOUIN <Damien BEAUDOUIN>
2017-03-02 18:02:35 +0100

    ajout de testSite

---
+++ testSite/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    100% statique<br>
+    <a href="?nb=2">Affichage 2 Hello</a><br>
+    <a href="?nb=5">Affichage 5 Hello</a><br>
+    <a href="?nb=15">Affichage 15 Hello</a><br>
+    <form>
+        <label for="nb">Nombre : </label>
+        <input id="nb" name="nb" type="number" max="200">
+    </form>
+<?php 
+    $texte="Hello World(dynamique)<br>";
+    $i=0;
+    $max=$_GET["nb"];
+    $max=min(200,$max);
+    while($i<$max){
+        $color="";
+        if($i%2==0)
+            $color="color:red;";
+        elseif($i%3==0)
+            $color="color:green;";
+        else
+            $color="color:darkblue;";
+        echo "<span style='{$color}font-size:{$i}pt;'>{$texte}</span>";
+        $i++;
+    }
+?>
+</body>
+</html>
